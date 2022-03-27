e73b2991a0a6f41f2f722ce12e33c66a7f07efe0
Thomas Gledhill <thomasjgledhill@gmail.com>
2022-03-27 15:27:52 +0100

    command name colour change

--- GnosticTest_0.0.2/control.lua
+++ GnosticTest_0.0.2/control.lua
@@ -3,7 +3,7 @@ function foo(tableIn)
     local player = game.get_player(tableIn.player_index)
     local color = {r=0, g=0, b=1, a=1}
 
-    player.print("This is a private test message")
+    player.print("This is a private test message to " .. player.name, color)
     game.print("This is a global test message")
 
 end
