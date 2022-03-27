43e2f3f1b12dc2228d65ff7d6af368fc65fe4c40
Thomas Gledhill <thomasjgledhill@gmail.com>
2022-03-27 15:53:38 +0100

    comments

--- GnosticTest_0.0.2/control.lua
+++ GnosticTest_0.0.2/control.lua
@@ -1,4 +1,4 @@
-function foo(tableIn)
+function foo(tableIn) -- this command adds a command "/test_message" that says some stuff in the chat
 
     local player = game.get_player(tableIn.player_index)
     local color = {r=0, g=0, b=1, a=1}
@@ -10,7 +10,7 @@ end
 
 commands.add_command("test_message", "This is a test command that sends a message", foo)
 
-function iron(tableIn)
+function iron(tableIn) --this command adds a command "/give_iron_plates" that gives the player 100 iron plates
 
     local player = game.get_player(tableIn.player_index)
     local ironplatestack = {name="iron-plate", count = 100}
