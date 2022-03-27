eb2c2eb344ff25d4238ce5d8b546bf5e36bf2430
Thomas Gledhill <thomasjgledhill@gmail.com>
2022-03-27 15:21:49 +0100

    0.0.2 : test command

---
+++ GnosticTest_0.0.2/control.lua
@@ -0,0 +1,11 @@
+function foo(tableIn)
+
+    local player = game.get_player(tableIn.player_index)
+    local color = {r=0, g=0, b=1, a=1}
+
+    player.print("This is a private test message")
+    game.print("This is a global test message")
+
+end
+
+commands.add_command("test_message", "This is a test command that sends a message", foo)
