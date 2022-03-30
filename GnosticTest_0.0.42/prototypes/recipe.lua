b8f24b1e31a9ea9fff15290829a3b1cac4960671
Paul Ranshaw <p.s.ranshaw@gmail.com>
2022-03-30 21:21:28 +0100

    Update recipe.lua

--- GnosticTest_0.0.42/prototypes/recipe.lua
+++ GnosticTest_0.0.42/prototypes/recipe.lua
@@ -9,6 +9,15 @@ data:extend({
         },
         result = "nuclear-furnace",
     },
+    { --nuclear inserter
+        type = "recipe",
+        name = "nuclear-inserter",
+        enabled = "true",
+        ingredients = {
+            {"stack-inserter", 1}, {"processing-unit", 5}, {"steel-plate", 25}
+        },
+        result = "nuclear-inserter",
+    }
 
 }
 )
